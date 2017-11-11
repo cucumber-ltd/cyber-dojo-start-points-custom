53fa5ddc139c2217883791451e9ed0275ee3ee83
Seb Rose <seb@claysnow.co.uk>
2017-11-11 16:22:11 -0500

    Removed unneeded callback parameter

--- shouty-js/coordinate.test.js
+++ shouty-js/coordinate.test.js
@@ -8,19 +8,19 @@ describe("coordinate class", () => {
   it("should calculate the distance from itself", () => {
     var a = new Coordinate(0, 0);
     assert.equal(a.distanceFrom(a), 0);
-  })
+  });
 
   it("should calculate the distance from another coordinate along X axis", ()=>{
     var a = new Coordinate(0, 0);
     var b = new Coordinate(600, 0);
 
     assert.equal(a.distanceFrom(b), 600);
-  })
+  });
 
   // it("should calculate the distance from another coordinate", ()=>{
   //   var a = new Coordinate(0, 0);
   //   var b = new Coordinate(300, 400);
   //
   //   assert.equal(a.distanceFrom(b), 500);
-  // })
-})
+  // });
+});
--- shouty-js/shout_steps.js
+++ shouty-js/shout_steps.js
@@ -9,23 +9,23 @@ defineSupportCode(function({Given, When, Then}) {
   const ARBITARY_MESSAGE = 'Hello, world';
   var shouty = new Shouty();
 
-  Given(/^Lucy is at (\d+), (\d+)$/, function (x, y, callback) {
+  Given(/^Lucy is at (\d+), (\d+)$/, function (x, y) {
     shouty.setLocation('Lucy', new Coordinate(x, y));
   });
 
-  Given(/^Sean is at (\d+), (\d+)$/, function (x, y, callback) {
+  Given(/^Sean is at (\d+), (\d+)$/, function (x, y) {
     shouty.setLocation('Sean', new Coordinate(x, y));
   });
 
-  When(/^Sean shouts$/, function (callback) {
+  When(/^Sean shouts$/, function () {
     shouty.shout('Sean', ARBITARY_MESSAGE);
   });
 
-  Then(/^Lucy should hear Sean$/, function (callback) {
+  Then(/^Lucy should hear Sean$/, function () {
     assert.equal(Object.keys(shouty.getShoutsHeardBy('Lucy')).length, 1);
   });
 
-  Then(/^Lucy should hear nothing$/, function (callback) {
+  Then(/^Lucy should hear nothing$/, function () {
     assert.equal(Object.keys(shouty.getShoutsHeardBy('Lucy')).length, 0);
   });
 
