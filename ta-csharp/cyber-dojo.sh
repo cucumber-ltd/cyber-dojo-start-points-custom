3d604518d0b1d12609ac2c902a714cf0d8cf0a82
Seb Rose <seb@claysnow.co.uk>
2017-11-21 13:52:49 +0000

    Fixed cyber-dojo.sh

--- ta-csharp/cyber-dojo.sh
+++ ta-csharp/cyber-dojo.sh
@@ -1,9 +1,12 @@
-NUNIT_PATH=/nunit/NUnit.2.6.2/lib
+NUNIT_PATH=/nunit/lib/net45
 export MONO_PATH=${NUNIT_PATH}
 
 set -e
 [ -f report.xml ] && rm report.xml
+export RELIABLE_CONNECTION
 export VOLATILE_STATS_DATA
+export FAKE_INITIALISATION_DATA #="19,123456.78;22,123456.78;57,123456.78"
+
 
 mcs -t:exe \
   -r:${NUNIT_PATH}/nunit.framework.dll \
@@ -14,6 +17,6 @@ mcs -t:library \
   -out:RunTests.dll *.cs
 
 if [ $? -eq 0 ]; then
-  NUNIT_RUNNERS_PATH=/nunit/NUnit.Runners.2.6.1/tools
-  mono ${NUNIT_RUNNERS_PATH}/nunit-console.exe -nologo ./RunTests.dll
+  NUNIT_RUNNERS_PATH=/nunit/tools
+  mono ${NUNIT_RUNNERS_PATH}/nunit3-console.exe --noheader ./RunTests.dll
 fi
