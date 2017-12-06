c50778578d1930601932eed65bb68ab067f6442b
Seb Rose <seb@claysnow.co.uk>
2017-12-06 15:41:54 +0000

    Updated shouty-rb to use Cucumber expressions

--- shouty-rb/shout_steps.rb
+++ shouty-rb/shout_steps.rb
@@ -6,22 +6,22 @@ Before do
   @shouty = Shouty.new
 end
 
-Given(/^Lucy is at (\d+), (\d+)$/) do |x, y|
+Given('Lucy is at {int}, {int}') do |x, y|
   @shouty.set_location("Lucy", Coordinate.new(x, y))
 end
 
-Given(/^Sean is at (\d+), (\d+)$/) do |x, y|
+Given('Sean is at {int}, {int}') do |x, y|
   @shouty.set_location("Sean", Coordinate.new(x, y))
 end
 
-When(/^Sean shouts$/) do
+When('Sean shouts') do
   @shouty.shout("Sean", ARBITRARY_MESSAGE)
 end
 
-Then(/^Lucy should hear Sean$/) do
-  expect(@shouty.shouts_heard_by("Lucy").length).to eq(1)
+Then('Lucy should hear Sean') do
+  expect(@shouty.shouts_heard_by("Lucy").size).to eq(1)
 end
 
-Then(/^Lucy should hear nothing$/) do
-  expect(@shouty.shouts_heard_by("Lucy")).to eq({})
+Then('Lucy should hear nothing') do
+  expect(@shouty.shouts_heard_by("Lucy").size).to eq(0)
 end
