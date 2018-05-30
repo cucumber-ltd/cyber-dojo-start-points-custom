d64c10786d48ef0e3835272d07c819d0a716cc3b
Seb Rose <github@claysnow.co.uk>
2018-05-30 12:48:19 +0100

    Reverted RunCukesTest

--- shouty-java3/RunCukesTest.java
+++ shouty-java3/RunCukesTest.java
@@ -4,7 +4,7 @@ import cucumber.api.SnippetType;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict=true, monochrome=true, plugin={ "pretty", "pretty, junit:/tmp/output" }, snippets=SnippetType.CAMELCASE)
+@CucumberOptions(strict=true, monochrome=true, plugin="junit:/tmp/output", snippets=SnippetType.CAMELCASE)
 public class RunCukesTest {
     // this is the adapter/bridge code
     // between cucumber jvm and junit
