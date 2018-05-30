6982ef23964dfe36673c107b4bd662a4099b53a3
Seb Rose <seb@claysnow.co.uk>
2018-05-30 11:00:40 +0100

    Fixed manifest again

--- shouty-java3/ShoutSteps.java
+++ shouty-java3/ShoutSteps.java
@@ -1,34 +1,37 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-import cucumber.api.java.en.*;
-import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static java.util.Collections.emptyMap;
+import static org.junit.Assert.assertEquals;
+
 
 public class ShoutSteps {
-    private final String ARBITRARY_MESSAGE = "Hello, world";
+    private static final String ARBITRARY_MESSAGE = "Hello, world";
     private final Shouty shouty = new Shouty();
 
-    @Given("^Lucy is at (\\d+), (\\d+)$")
-    public void lucyIsAt(int xCoord, int yCoord) throws Throwable {
+    @Given("Lucy is at {int}, {int}")
+    public void lucy_is_at(int xCoord, int yCoord) {
         shouty.setLocation("Lucy", new Coordinate(xCoord, yCoord));
     }
 
-    @Given("^Sean is at (\\d+), (\\d+)$")
-    public void seanIsAt(int xCoord, int yCoord) throws Throwable {
+    @Given("Sean is at {int}, {int}")
+    public void sean_is_at(int xCoord, int yCoord) {
         shouty.setLocation("Sean", new Coordinate(xCoord, yCoord));
     }
 
-    @When("^Sean shouts$")
-    public void seanShouts() throws Throwable {
+    @When("Sean shouts")
+    public void sean_shouts() {
         shouty.shout("Sean", ARBITRARY_MESSAGE);
     }
 
-    @Then("^Lucy should hear Sean$")
-    public void lucyShouldHearHim() throws Throwable {
-        Assert.assertEquals(1, shouty.getShoutsHeardBy("Lucy").size());
+    @Then("Lucy should hear Sean")
+    public void lucy_should_hear_sean() {
+        assertEquals(1, shouty.getShoutsHeardBy("Lucy").size());
     }
 
-    @Then("^Lucy should hear nothing$")
-    public void lucy_should_hear_nothing() throws Throwable {
-        Assert.assertEquals(0, shouty.getShoutsHeardBy("Lucy").size());
+    @Then("Lucy should hear nothing")
+    public void lucy_should_hear_nothing() {
+        assertEquals(emptyMap(), shouty.getShoutsHeardBy("Lucy"));
     }
 }
