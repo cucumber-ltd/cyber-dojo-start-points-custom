69bd7654d8ca9931ff077e8e34ef38055997f7a5
Seb Rose <seb@claysnow.co.uk>
2021-07-15 16:13:46 +0100

    Fixed cyber-dojo.sh

--- print-diamond-js/cyber-dojo.sh
+++ print-diamond-js/cyber-dojo.sh
@@ -1,26 +1,26 @@
+# set the version to use:
+#NODE_VERSION=0.12.7
+#NODE_VERSION=4.1.1
+#NODE_VERSION=4.2.1
+#NODE_VERSION=6.11.1
+#NODE_VERSION=8.2.1
 NODE_VERSION=8.4.0
+#NODE_VERSION=9.4.0
+#NODE_VERSION=9.10.1
+#NODE_VERSION=10.1.0
+#NODE_VERSION=13.7.0
+#NODE_VERSION=14.1.0
 
-echo JASMINE TESTS
-echo =============
+# Use npm package 'n' to call jshint and jasmine with selected node version:
+if [ -f .jshintrc ]; then
+  n use $NODE_VERSION \
+    /usr/lib/node_modules/jshint/bin/jshint \
+    --config .jshintrc \
+    *.js
+fi
 
-n use ${NODE_VERSION} /usr/local/lib/node_modules/jasmine/bin/jasmine \
-  JASMINE_CONFIG_PATH=tests/jasmine.json
-
-# You have to cd to a specific folder because
-#   ~/node_modules/cucumber/bin/cucumber-js
-# contains the line
-# require('../lib/cli/run.js').default();
-# Go figure!
-
-cd ~/node_modules/cucumber/bin
-
-export NODE_PATH=/home/cucumber/node_modules
-
-echo 
-echo CUCUMBER FEATURES
-echo =================
-
-./cucumber-js \
-  --format-options '{"colorsEnabled":false}' \
-  --format-options '{"snippetInterface":"synchronous"}' \
-  ${CYBER_DOJO_SANDBOX}/features/*.feature
+if [ $? == 0 ]; then
+  n use $NODE_VERSION \
+    /usr/local/lib/node_modules/jasmine/bin/jasmine \
+    JASMINE_CONFIG_PATH=./jasmine.json
+fi
